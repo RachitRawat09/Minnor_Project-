912b0438544bc02e4c6f498623d266b8df652122
RachitRawat09 <rawatritik5670@gmail.com>
2025-02-12 22:21:00 +0530

    Upload db Connection and Header

---
+++ includes/db_connect.php
@@ -0,0 +1,19 @@
+<?php
+// Database configuration
+$host = "localhost";   // Server (Keep 'localhost' for local development)
+$username = "root";    // Default username for XAMPP/MAMP/WAMP
+$password = "";        // Leave empty if no password is set
+$database = "restaurant_menu";  // Change this to your database name
+$port=3307;
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database,$port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Set character encoding to avoid issues with special characters
+$conn->set_charset("utf8");
+
+?>
